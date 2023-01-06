3e3a25380030aff14d97feb4a40c3933aab212ae
Ziggybaba1 <seyiadejugbagbe@gmail.com>
2023-01-06 04:11:50 +0100

    final submit

---
+++ src/server.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles, isImage } from "./util/util";
+
+(async () => {
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+  app.get("/api/v0/filtered_image", async (req: Request, res: Response) => {
+    let { image_url } = req.query;
+    let checkImageUrl = await isImage(image_url);
+    let arrayFilePathUrl: Array<string> = [];
+    if (image_url && checkImageUrl) {
+      let response = await filterImageFromURL(image_url);
+      arrayFilePathUrl.push(response);
+      res.sendFile(response, async function (err) {
+        if (err) {
+        } else {
+          await deleteLocalFiles(arrayFilePathUrl);
+        }
+      });
+    } else {
+      res.status(400).send("not implemented");
+    }
+  });
+  //! END @TODO1
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
+    res.send("");
+  });
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
